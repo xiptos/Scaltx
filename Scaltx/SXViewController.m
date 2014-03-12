6f00e9e47874863e65a17adf5daea2e9179cf65d
rlopes <rlopes@ipb.pt>
2014-03-12 16:11:40 +0000

    Initial art

--- Scaltx/SXViewController.m
+++ Scaltx/SXViewController.m
@@ -12,7 +12,11 @@
 
 @end
 
-@implementation SXViewController
+@implementation SXViewController {
+    double speed1;
+    double previousTime;
+    double previousPosition;
+}
 
 - (void)viewDidLoad
 {
@@ -24,6 +28,51 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    UITouch * touch=[touches anyObject];
+    CGPoint touchPosition=[touch locationInView:self.view];
+    speed1 = 0;
+    previousTime = CACurrentMediaTime();
+    previousPosition = touchPosition.x;
+    
+    NSLog(@"TouchesBegan: %f,%f",touchPosition.x, touchPosition.y);
+//    for(int i=0;i<9;i++) {
+//        if(CGRectContainsPoint(_boardPositions[i].frame, touchPosition)) {
+//            [_board playForPosition:i];
+//            [self updateBoard];
+//        }
+//    }
+}
+
+- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event {
+    UITouch * touch=[touches anyObject];
+    CGPoint touchPosition=[touch locationInView:self.view];
+    
+    double currentTime = CACurrentMediaTime();
+    double currentPosition = touchPosition.x;
+    
+    speed1 = (currentPosition - previousPosition) / (currentTime - previousTime);
+    
+    previousPosition = currentPosition;
+    previousTime = currentTime;
+    NSLog(@"Speed: %f",speed1);
+    
+}
+
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
+    UITouch * touch=[touches anyObject];
+    CGPoint touchPosition=[touch locationInView:self.view];
+    NSLog(@"TouchesEnded: %f,%f",touchPosition.x, touchPosition.y);
+    
+}
+
+- (void)touchesCancelled:(NSSet *)touches withEvent:(UIEvent *)event {
+    UITouch * touch=[touches anyObject];
+    CGPoint touchPosition=[touch locationInView:self.view];
+    NSLog(@"TouchesCancelled: %f,%f",touchPosition.x, touchPosition.y);
+    
 }
 
 @end
